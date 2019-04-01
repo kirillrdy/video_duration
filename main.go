ead415f3c9801dd603fe3cd5551b15c021e285f2
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2019-04-01 15:52:49 +1100

    flag for removing things

--- main.go
+++ main.go
@@ -1,22 +1,32 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/anacrolix/ffprobe"
-	"log"
+	"os"
 	"path/filepath"
 	"time"
 )
 
 func main() {
+
+	remove := flag.Bool("rm", false, "Removes videos longer than 1h")
+	flag.Parse()
+
 	var sum time.Duration
 	files, _ := filepath.Glob("*.mp4")
 	for _, file := range files {
-		info, _ := ffprobe.Run(file)
-
-		duration, _ := info.Duration()
-		log.Print(duration)
-		sum += duration
+		info, err := ffprobe.Run(file)
+		if err == nil {
+			duration, _ := info.Duration()
+			if *remove == true && duration > time.Hour {
+				os.Remove(file)
+			}
+			fmt.Printf("%s %s\n", file, duration)
+			sum += duration
+		}
 	}
 
-	log.Print(sum)
+	fmt.Printf("Total: %s\n", sum)
 }
